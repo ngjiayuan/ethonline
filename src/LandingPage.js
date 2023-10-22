d2e7d92dac2f0e26760a761b64ee0082a9a91743
Ryan Jun Xiong Tan <ryan.tanjunxiong@gmail.com>
2023-10-22 15:13:52 +0200

    some landing page clean up

---
+++ src/LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <div className="center-content">
+        <h1>Welcome to the App</h1>
+        <p>Sign in to get started</p>
+        {/* Add a Sign-In button or form here */}
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
