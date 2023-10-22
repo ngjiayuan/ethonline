b9041ed670c058a56291f9951185e371302f2989
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2023-10-22 09:25:13 -0400

    feat: update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import LandingPage from './LandingPage';
 import NavigationBar from './NavBar'; // Import the NavigationBar component
+import Login from './Login';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <NavigationBar /> {/* Add the NavigationBar component here */}
+      <Login />
       <LandingPage />
       {/* Add other app routes/components here */}
     </div>
